cc743d9405bf91bf7bf87f1413acfd758dc93e6c
Dylan Iván Rodriguez Sánchez <23470244@Campeche.tecnm.mx>
2025-10-29 11:46:01 -0600

    Ejemplo de sicronización de elementos XAML con manejo de eventos

--- DataBinding.XamlElement/MainPage.xaml.cs
+++ DataBinding.XamlElement/MainPage.xaml.cs
@@ -7,7 +7,14 @@
         public MainPage()
         {
             InitializeComponent();
+            EnteredTextLabel.Text = string.Empty;
         }
+
+       private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnteredTextLabelText.Text= string.Empty;
+        }
+
 
     }
 }
