b1f9cb4d1db5cb7b847fbc05f957748acc6f4959
Dylan <dylansanchez357@gmail.com>
2025-11-05 13:35:19 -0600

    Copia de DataBinding

---
+++ DataBinding.Coleccion/Models/OrigenDePaquete.cs
@@ -0,0 +1,15 @@
+﻿
+namespace DataBinding.Coleccion.Models
+{
+    class OrigenDePaquete
+    {
+        public string Nombre {  get; set; } = string.Empty;
+        public string Origen { get; set; } = string.Empty;
+        public bool EstaHabilitado { get; set; } = false;
+
+        public override string ToString()
+        {
+            return $"{Nombre} - ( {Origen} )";
+        }
+    }
+}
--- DataBinding.Coleccion/Views/MainPage.xaml.cs
+++ DataBinding.Coleccion/Views/MainPage.xaml.cs
@@ -1,9 +1,57 @@
+using DataBinding.Coleccion.Models;
+
 namespace DataBinding.Coleccion.Views;
 
 public partial class MainPage : ContentPage
 {
+	private List<OrigenDePaquete> _origenes;
 	public MainPage()
 	{
 		InitializeComponent();
+		_origenes=new List<OrigenDePaquete>();
+		CargarDatos();
+		OrigenesListView.ItemsSource = _origenes;
 	}
+
+	private void CargarDatos()
+	{
+		_origenes.Add(new OrigenDePaquete
+		{
+			Nombre = "nuget.org",
+			Origen = "https://api.nuget.org/v3/index.json",
+			EstaHabilitado = true
+		});
+        _origenes.Add(new OrigenDePaquete
+        {
+            Nombre = "Microsoft Visual Studio Offline Packages",
+            Origen = @"C:\Program Files(x86)\Microsoft SDKs\NuGetPackages",
+            EstaHabilitado = false
+        });
+    }
+
+    private void OnAgregarButtonClicked(object sender, EventArgs e)
+    {
+        _origenes.Add(new OrigenDePaquete
+        {
+            Nombre = "Origen del paquete",
+            Origen = "URL o ruta del origen del paquete",
+            EstaHabilitado = false
+        });
+        OrigenesListView.ItemsSource = null;
+        OrigenesListView.ItemsSource = _origenes;
+    }
+
+    private void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        OrigenDePaquete seleccionado = 
+            (OrigenDePaquete)OrigenesListView.SelectedItem;
+        if (seleccionado != null)
+        {
+            _origenes.Remove(seleccionado);
+            OrigenesListView.ItemsSource = null;
+            OrigenesListView.ItemsSource = _origenes;
+
+        }
+
+    }
 }
